b64ff68c833387d9dcea5c6a7ad9ab7214e4f4a5
Essam21 <essam20211061@protonmail.com>
2021-11-17 21:35:02 +0200

    Upload Disney Plus video downloader

---
+++ download.audio.bat
@@ -0,0 +1,3 @@
+@echo off
+disneyplus.py --nv --ns --keep --url https://www.disneyplus.com/es-es/series/loki/6pARMvILBGzF -s 1 -e 1 --alang eng
+pause
---
+++ download.video.bat
@@ -0,0 +1,3 @@
+@echo off
+disneyplus.py --url https://www.disneyplus.com/es-es/series/loki/6pARMvILBGzF -q 720 -s 1 -e 1 --alang eng --slang eng --flang eng
+pause
